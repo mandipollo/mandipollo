bbe9e78b92f7559782153a4af3c4118bcc13b7e5
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2024-09-29 11:26:28 +0100

    update svg

--- src/components/homepage/Introduction.tsx
+++ src/components/homepage/Introduction.tsx
@@ -20,7 +20,7 @@ const Introduction = () => {
 							<span className="h-6 w-[1px] bg-gradient-to-r from-green-400 to-cyan-400  -translate-y-1 translate-x-4 rotate-90"></span>
 							<span className="h-4 w-[1px] bg-gradient-to-r from-pink-400 to-cyan-400 -translate-y-2 translate-x-5 -rotate-45"></span>
 						</div>
-						<span className="h-2 w-10 bg-gradient-to-r from-green-400 to-cyan-400"></span>
+						<span className="h-2 w-6 md:w-20 bg-gradient-to-r from-green-400 to-cyan-400"></span>
 					</div>
 				</div>
 				<div>
@@ -36,7 +36,7 @@ const Introduction = () => {
 					<div className="flex space-x-2">
 						<span className="h-2 w-20 bg-gradient-to-r from-purple-400 to-orange-400"></span>
 						<span className="h-2 w-2 bg-gradient-to-r rounded-full from-slate-400 to-slate-400"></span>
-						<span className="h-2 w-60 bg-gradient-to-r from-green-400 to-cyan-400"></span>
+						<span className="h-2 w-40 md:w-60 bg-gradient-to-r from-green-400 to-cyan-400"></span>
 					</div>
 					<div className="flex space-x-2">
 						<span className="h-2 w-10 bg-gradient-to-r from-green-400 to-cyan-400"></span>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,16 +11,10 @@ const HomePage: FC = () => {
 
 	const pathRef = useRef<SVGPathElement>(null);
 
-	// window.addEventListener("scroll", () => {
-	// 	console.log("scroll");
-	// });
-	// main issue the scroll listener does not fire event
-
 	useEffect(() => {
 		const handleScroll = () => {
 			if (pathRef.current) {
 				const length = pathRef.current.getTotalLength();
-
 				setPathLength(length);
 				setDashOffset(length);
 				const scrollPercentage =
@@ -46,58 +40,31 @@ const HomePage: FC = () => {
 		<main className="flex h-full flex-col  text-white ">
 			<div className=" fixed top-0 left-0 w-full h-full text-center pointer-events-none">
 				<svg
-					viewBox="0 0 395 2336"
 					fill="none"
 					preserveAspectRatio="xMidYMax meet"
 					className="inline-block h-full w-full"
+					viewBox="0 0 2261 7004"
 				>
-					<g filter="url(#filter0_d_1_7)">
-						<path
-							style={{ strokeDashoffset: dashOffset }}
-							strokeDasharray={`${pathLength}  ${pathLength}`}
-							ref={pathRef}
-							strokeWidth={10}
-							d="M209 0V206C222.662 242.951 231.997 251.103 251 248C277.655 237.934 280.834 229.496 272 211C211.965 204.543 175.818 220.146 179 253C195.819 250.77 203.699 249.485 209 226V296V359C159.867 421.484 164.369 456.516 209 519C261.393 587.67 260.784 618.844 209 658C157.319 722.648 153.781 755.715 209 807C284.859 884.372 282.545 930.955 209 1019C157.417 1108.19 159.364 1159.24 209 1249V1583V1854C233.501 1919.21 257.835 1942.22 321 1958C380.806 1945.89 394.254 1921.89 389 1854C246.611 1847.03 183.117 1873.47 90 1958C11.5057 1955.05 -7.38621 1933.7 12 1854C244.332 1773.36 325.497 1816.99 389 2042C302.633 2098.5 134.338 2074.01 209 2127C229.35 2244.35 226.429 2282.51 209 2327"
-							stroke="#F24040"
-							shapeRendering="geometricPrecision"
-						/>
-					</g>
+					<path
+						style={{ strokeDashoffset: dashOffset }}
+						strokeDasharray={`${pathLength}  ${pathLength}`}
+						ref={pathRef}
+						strokeWidth={20}
+						d="M1112.5 0V228C1169.44 415.361 1164.76 495.253 1112.5 605C1040.57 769.997 1043.23 862.503 1112.5 1027.5C1245.29 1306.11 1246.48 1525.96 1112.5 1742.5C883.911 2101.64 959.64 2317.98 1112.5 2648C1218.25 2871.25 1208.61 2913.05 1112.5 3144C1040.87 3292.22 1047.05 3379.23 1112.5 3539C1161.24 3663.08 1157.61 3730.11 1112.5 3846.5L990 4058H1230.5L1112.5 3860V4253V4588L684.5 4808.5L1112.5 4949L1507.5 4808.5L1112.5 4658.5L882 4808.5L1112.5 4866L1230.5 4808.5L1112.5 4738.5V5552L623.5 5938L362 5552L1472.5 5938L1852 5552L1112.5 5938L1.5 5552L362 6452L1472.5 6247.5L2260.5 5552L1112.5 6107.5V7004 "
+						stroke="url(#paint0_linear_1_6)"
+					/>
 					<defs>
-						<filter
-							id="filter0_d_1_7"
-							x="0.450378"
-							y="0"
-							width="394.09"
-							height="2335.18"
-							filterUnits="userSpaceOnUse"
-							colorInterpolationFilters="sRGB"
+						<linearGradient
+							id="paint0_linear_1_6"
+							x1="1084.26"
+							y1="228"
+							x2="1084.26"
+							y2="3846.5"
+							gradientUnits="userSpaceOnUse"
 						>
-							<feFlood floodOpacity="0" result="BackgroundImageFix" />
-							<feColorMatrix
-								in="SourceAlpha"
-								type="matrix"
-								values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-								result="hardAlpha"
-							/>
-							<feOffset dy="4" />
-							<feGaussianBlur stdDeviation="2" />
-							<feComposite in2="hardAlpha" operator="out" />
-							<feColorMatrix
-								type="matrix"
-								values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-							/>
-							<feBlend
-								mode="normal"
-								in2="BackgroundImageFix"
-								result="effect1_dropShadow_1_7"
-							/>
-							<feBlend
-								mode="normal"
-								in="SourceGraphic"
-								in2="effect1_dropShadow_1_7"
-								result="shape"
-							/>
-						</filter>
+							<stop stop-color="#55B855" />
+							<stop offset="1" stop-color="#4AB6E7" />
+						</linearGradient>
 					</defs>
 				</svg>
 			</div>
